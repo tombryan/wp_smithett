9279a26397d17a0cd7193c43f14f6e1fb72e8ea5
Tom Bryan <hello@tombryan.co>
2014-03-25 17:22:51 +1100

    add jquery mobile menu

--- functions.php
+++ functions.php
@@ -88,7 +88,11 @@ function wp_smithett_scripts() {
 
 	wp_enqueue_script( 'wp_smithett-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+  wp_enqueue_script( 'wp_smithett-mobile-menu', get_template_directory_uri() . '/vendor/javascripts/jquery.mmenu.min.all.js', array( 'jquery' ), '20140325', true );
+
+	wp_enqueue_script( 'wp_smithett-menu', get_template_directory_uri() . '/js/mobile-menu.js', array( 'wp_smithett-mobile-menu' ), '20140325', true );
+
+  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
